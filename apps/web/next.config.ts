4f007dcab679dc5492637b333e17dccf1dffd019
Jack Maders <jackwmaders@gmail.com>
2026-02-20 21:56:53 +0000

    feat: update payloadcms config

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,3 +1,8 @@
 import { withPayload } from "@payloadcms/next/withPayload";
+import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
+
+if (process.env.NODE_ENV === 'development') {
+  initOpenNextCloudflareForDev();
+}
 
 export default withPayload({});
--- apps/web/payload.config.ts
+++ apps/web/payload.config.ts
@@ -1,19 +1,24 @@
-import { buildConfig } from 'payload';
-import { sqliteD1Adapter } from '@payloadcms/db-d1-sqlite';
+import {
+	getCloudflareContext,
+} from "@opennextjs/cloudflare";
+import { sqliteD1Adapter } from "@payloadcms/db-d1-sqlite";
+import { buildConfig } from "payload";
+
+const cloudflare = await getCloudflareContext({ async: true });
 
 export default buildConfig({
-  secret: process.env.PAYLOAD_SECRET || 'super-secret-local-key',
-  admin: {
-    user: 'users',
-  },
-  collections: [
-    {
-      slug: 'users',
-      auth: true,
-      fields: [],
-    },
-  ],
-  db: sqliteD1Adapter({
-    binding: process.env.DB || 'DB', 
-  }),
+	secret: process.env.PAYLOAD_SECRET || "super-secret-local-key",
+	admin: {
+		user: "users",
+	},
+	collections: [
+		{
+			slug: "users",
+			auth: true,
+			fields: [],
+		},
+	],
+	db: sqliteD1Adapter({
+		binding: cloudflare.env.payload_db,
+	}),
 });
